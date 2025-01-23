7cce857894c8bfe1fc82bd6ef70b26f530289583
RYUL99 <seunglul1999@gmail.com>
2025-01-23 11:22:12 +0900

    Initial commit

---
+++ src/main/java/com/moyo/backend/MoyoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moyo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/moyobackend/MoyoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.moyobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
