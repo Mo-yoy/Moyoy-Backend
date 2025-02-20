719d20ae89796c92fdeb8e6f1d24c16a80717b28
RYUL99 <seunglul1999@gmail.com>
2025-02-20 18:27:28 +0900

    add response-format

--- src/main/java/com/moyo/backend/common/constant/MoyoConstants.java
+++ src/main/java/com/moyo/backend/common/constant/MoyoConstants.java
@@ -5,4 +5,15 @@ public class MoyoConstants {
     public static final String JWT = "JWT";
     public static final String BEARER = "Bearer";
 
+    public static final int OK = 200;
+    public static final int NO_CONTENT = 204;
+    public static final int FOUND = 302;
+    public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
+    public static final int NOT_FOUND = 404;
+    public static final int METHOD_NOT_ALLOWED = 405;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+
+
 }
