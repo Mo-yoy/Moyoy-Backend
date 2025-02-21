1a48385533453a51610e3395ef8263a3ec2efa29
RYUL99 <seunglul1999@gmail.com>
2025-02-21 19:03:11 +0900

    Add SecurityConfig, MVCCorsConfig

---
+++ src/main/java/com/moyo/backend/security/config/OAuth2LoginSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.moyo.backend.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class OAuth2LoginSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/reissue/token", "/health", "/").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(auth-> auth
+                                .baseUri("/auth/login")
+                        )
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService()
+                        )
+                        .successHandler()
+                        .failureHandler()
+                );
+
+        return http.build();
+    }
+}
