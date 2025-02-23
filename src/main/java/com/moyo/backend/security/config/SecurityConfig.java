d3444c0d6daeed15b36d91269133a53f80966482
RYUL99 <seunglul1999@gmail.com>
2025-02-23 15:30:02 +0900

    작동만 하는 로그인 모듈 만들었음

--- src/main/java/com/moyo/backend/security/config/SecurityConfig.java
+++ src/main/java/com/moyo/backend/security/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class OAuth2LoginSecurityConfig {
 
---
+++ src/main/java/com/moyo/backend/security/oauth/dto/GithubUserProfile.java
@@ -0,0 +1,31 @@
+package com.moyo.backend.security.oauth.dto;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class GithubProfile {
+
+    private static final String GITHUB_OAUTH_APP_USER_ID = "id";
+    private static final String GITHUB_USER_PROFILE_IMAGE_URL = "avatar_url";
+    private static final String GITHUB_USER_TAG = "login";
+
+
+    private final Map<String, Object> attributes;
+
+    public String getProviderId() {
+
+        return attributes.get(GITHUB_OAUTH_APP_USER_ID).toString();
+    }
+
+    public String getProfileImgUrl() {
+
+        return attributes.get(GITHUB_USER_PROFILE_IMAGE_URL).toString();
+    }
+
+    public String getUsername() {
+
+        return attributes.get(GITHUB_USER_TAG).toString();
+    }
+}
