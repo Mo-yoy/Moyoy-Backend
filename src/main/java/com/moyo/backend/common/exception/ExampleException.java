0c8ae05c9cac66c1d66d5136c7aebcf02fe1e98e
RYUL99 <seunglul1999@gmail.com>
2025-03-01 20:38:30 +0900

    ExampleException 삭제

--- src/main/java/com/moyo/backend/common/exception/ExampleException.java
+++
@@ -1,9 +0,0 @@
-package com.moyo.backend.common.exception;
-
-public class ExampleException extends MoyoException{
-
-    public static final MoyoException EXCEPTION = new ExampleException();
-    private ExampleException() {
-        super(CommonErrorCode.UNKNOWN_INTERNAL_SERVER_ERROR);
-    }
-}
