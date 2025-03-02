2a5fbf53d421ca8e635fac68e4b5a19cee625ddf
SeungRyul <100277876+seungryul99@users.noreply.github.com>
2025-03-02 12:49:02 +0900

    Feat: Add GlobalExceptionHandler (#26)
    * GlobalExceptionHandler 1차 구현
    
    * ExampleException 삭제

--- src/main/java/com/moyo/backend/common/exception/CommonErrorCode.java
+++ src/main/java/com/moyo/backend/common/exception/CommonErrorCode.java
@@ -3,13 +3,17 @@ package com.moyo.backend.common.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import static com.moyo.backend.common.constant.MoyoConstants.SERVER_ERROR;
+import static com.moyo.backend.common.constant.MoyoConstants.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements BaseErrorCode{
 
     // 예시
+    INVALID_PARAM(BAD_REQUEST, "COMMON_400_1", "파라미터를 다시 확인해 주세요."),
+    PARAM_TYPE_MISMATCH(BAD_REQUEST, "COMMON_400_2", "파라미터의 타입을 다시 확인해 주세요."),
+    RESOURCE_NOT_FOUND(NOT_FOUND, "COMMON_404_1", "리소스를 찾을 수 없습니다."),
+    NOT_ALLOWED_METHOD(METHOD_NOT_ALLOWED, "COMMON_405_1", "허용되지 않은 메서드 입니다."),
     UNKNOWN_INTERNAL_SERVER_ERROR(SERVER_ERROR, "COMMON_500_1", "서버 내부에서 알수 없는 에러가 발생 했습니다. 관리자에게 문의해 주세요.")
     ;
 
@@ -18,7 +22,7 @@ public enum CommonErrorCode implements BaseErrorCode{
     private final String message;
 
     @Override
-    public ErrorReason getErrorReason() {
+        public ErrorReason getErrorReason() {
         return new ErrorReason(status,code,message);
     }
 }
