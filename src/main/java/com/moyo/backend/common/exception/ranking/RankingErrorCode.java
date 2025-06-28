af7055e396918b906b0113ceefe230b37027df03
KimSeungRyul <seunglul1999@gmail.com>
2025-06-28 15:12:11 +0900

    Refactor: 랭킹 에러 코드 관리

---
+++ src/main/java/com/moyo/backend/common/exception/ranking/RankingErrorCode.java
@@ -0,0 +1,25 @@
+package com.moyo.backend.common.exception.ranking;
+
+import static com.moyo.backend.common.constant.MoyoConstants.*;
+
+import com.moyo.backend.common.exception.BaseErrorCode;
+import com.moyo.backend.common.exception.ErrorReason;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RankingErrorCode implements BaseErrorCode {
+
+	RANKING_NOT_EXIST(NOT_FOUND, "RANKING_404_1", "사용자의 랭킹을 찾을 수 없습니다.");
+
+	private final Integer status;
+	private final String code;
+	private final String message;
+
+	@Override
+	public ErrorReason getErrorReason() {
+		return new ErrorReason(status, code, message);
+	}
+}
