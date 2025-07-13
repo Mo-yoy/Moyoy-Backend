15daa53302990beba22e867ac5f3e85dc93257eb
KimSeungRyul <seunglul1999@gmail.com>
2025-07-13 14:27:20 +0900

    refactor: User Entity id 수정

--- src/main/java/com/moyo/backend/domain/user/implement/User.java
+++ src/main/java/com/moyo/backend/domain/user/implement/User.java
@@ -2,6 +2,8 @@ package com.moyo.backend.domain.user.implement;
 
 import static com.moyo.backend.common.constant.MoyoConstants.*;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,13 +26,16 @@ import jakarta.persistence.Table;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
 
-	// ID는 직접 관리
 	@Id
 	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false)
 	private String username;
+
+	@Column(nullable = false, unique = true)
+	private Integer githubUserId;
 
 	@Column(nullable = false)
 	private String profileImgUrl;
@@ -39,17 +44,19 @@ public class User extends BaseTimeEntity {
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
-	@Builder(access = AccessLevel.PRIVATE)
-	public User(Long id, String username, String profileImgUrl, Role role) {
+	@Builder
+	public User(Long id, Integer githubUserId, String username, String profileImgUrl, Role role) {
 		this.id = id;
+		this.githubUserId = githubUserId;
 		this.username = username;
 		this.profileImgUrl = profileImgUrl;
 		this.role = role;
 	}
 
+	@Deprecated
 	public static User from(OAuth2User oAuth2User) {
 		return User.builder()
-			.id(Long.parseLong(oAuth2User.getAttribute(GITHUB_OAUTH2_USER_ID).toString()))
+			.githubUserId(Long.parseLong(oAuth2User.getAttribute(GITHUB_OAUTH2_USER_ID).toString()))
 			.username(oAuth2User.getAttribute(GITHUB_OAUTH2_USER_NAME))
 			.profileImgUrl(oAuth2User.getAttribute(GITHUB_OAUTH2_USER_AVATAR_URL))
 			.build();
