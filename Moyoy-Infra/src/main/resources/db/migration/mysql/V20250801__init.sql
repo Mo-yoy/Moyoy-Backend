0faf468d752f17081dd6bc6299f57dc069347f6f
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-08-20 16:16:24 +0900

    Setting: Add flyway (#113)
    * Setting: flyway 초기 세팅
    
    * test 프로필 init.sql 추가
    
    * db 이름 수정

---
+++ Moyoy-Infra/src/main/resources/db/migration/mysql/V20250801__init.sql
@@ -0,0 +1,54 @@
+USE `moyoy`;
+
+DROP TABLE IF EXISTS rankings; # ranking에 user_id FK
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS oauth2_client_registration;
+DROP TABLE IF EXISTS jwt_access_token;
+
+CREATE TABLE users (
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    github_user_id INT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    profile_img_url VARCHAR(255) NOT NULL,
+    role ENUM ('ADMIN', 'USER') NOT NULL,
+    created_at DATETIME(6) NULL,
+    modified_at DATETIME(6) NULL,
+    CONSTRAINT UK_github_user UNIQUE (github_user_id)
+);
+
+CREATE TABLE rankings
+(
+    ranking_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id       BIGINT       NULL,
+    monthly_point BIGINT       NOT NULL,
+    weekly_point  BIGINT       NOT NULL,
+    yearly_point  BIGINT       NOT NULL,
+    grade         VARCHAR(255) NULL,
+    created_at    DATETIME(6)  NULL,
+    modified_at   DATETIME(6)  NULL,
+    CONSTRAINT fk_rankings_user
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE oauth2_authorized_client
+(
+    client_registration_id  VARCHAR(100)   NOT NULL,
+    principal_name          VARCHAR(200)   NOT NULL,
+    access_token_type       VARCHAR(100)   NOT NULL,
+    access_token_value      BLOB           NOT NULL,
+    access_token_issued_at  TIMESTAMP      NOT NULL,
+    access_token_expires_at TIMESTAMP      NOT NULL,
+    access_token_scopes     VARCHAR(1000)  NULL,
+    refresh_token_value     BLOB           NULL,
+    refresh_token_issued_at TIMESTAMP      NULL,
+    PRIMARY KEY (client_registration_id, principal_name)
+);
+
+CREATE TABLE jwt_refresh_token
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT      NOT NULL,
+    token_hash VARCHAR(44) NOT NULL,
+    expires_at TIMESTAMP   NOT NULL,
+    CONSTRAINT uq_token_hash UNIQUE (token_hash)
+);
