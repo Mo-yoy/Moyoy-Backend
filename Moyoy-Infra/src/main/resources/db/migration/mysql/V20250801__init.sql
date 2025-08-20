9d042f9800ee1274364990729b29da26d9e159e9
KimSeungRyul <seunglul1999@gmail.com>
2025-08-20 16:03:33 +0900

    test 프로필 init.sql 추가

--- Moyoy-Infra/src/main/resources/db/migration/mysql/V20250801__init.sql
+++ Moyoy-Infra/src/main/resources/db/migration/mysql/V20250801__init.sql
@@ -1,15 +1,22 @@
-CREATE TABLE IF NOT EXISTS `users` (
-                                       user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                       github_user_id INT NOT NULL,
-                                       username VARCHAR(255) NOT NULL,
+USE `moyoy-test`;
+
+DROP TABLE IF EXISTS rankings; # ranking에 user_id FK
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS oauth2_client_registration;
+DROP TABLE IF EXISTS jwt_access_token;
+
+CREATE TABLE users (
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    github_user_id INT NOT NULL,
+    username VARCHAR(255) NOT NULL,
     profile_img_url VARCHAR(255) NOT NULL,
     role ENUM ('ADMIN', 'USER') NOT NULL,
     created_at DATETIME(6) NULL,
     modified_at DATETIME(6) NULL,
     CONSTRAINT UK_github_user UNIQUE (github_user_id)
-    );
+);
 
-CREATE TABLE IF NOT EXISTS rankings
+CREATE TABLE rankings
 (
     ranking_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id       BIGINT       NULL,
@@ -21,9 +28,9 @@ CREATE TABLE IF NOT EXISTS rankings
     modified_at   DATETIME(6)  NULL,
     CONSTRAINT fk_rankings_user
     FOREIGN KEY (user_id) REFERENCES users(user_id)
-    );
+);
 
-CREATE TABLE IF NOT EXISTS oauth2_authorized_client
+CREATE TABLE oauth2_authorized_client
 (
     client_registration_id  VARCHAR(100)   NOT NULL,
     principal_name          VARCHAR(200)   NOT NULL,
@@ -35,14 +42,13 @@ CREATE TABLE IF NOT EXISTS oauth2_authorized_client
     refresh_token_value     BLOB           NULL,
     refresh_token_issued_at TIMESTAMP      NULL,
     PRIMARY KEY (client_registration_id, principal_name)
-    );
+);
 
-CREATE TABLE IF NOT EXISTS jwt_refresh_token
+CREATE TABLE jwt_refresh_token
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id    BIGINT      NOT NULL,
     token_hash VARCHAR(44) NOT NULL,
     expires_at TIMESTAMP   NOT NULL,
     CONSTRAINT uq_token_hash UNIQUE (token_hash)
-    );
-
+);
