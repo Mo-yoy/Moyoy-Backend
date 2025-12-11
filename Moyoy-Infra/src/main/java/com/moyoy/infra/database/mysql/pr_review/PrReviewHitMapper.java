49a59f01e724d62d7555c53de9ff9a01919fea29
zakie <realzzaekkii@gmail.com>
2025-12-12 03:47:12 +0900

    feat: 도메인 모델 - 엔티티 변환 매퍼 구현

---
+++ Moyoy-Infra/src/main/java/com/moyoy/infra/database/mysql/pr_review/PrReviewHitMapper.java
@@ -0,0 +1,24 @@
+package com.moyoy.infra.database.mysql.pr_review;
+
+import com.moyoy.domain.pr_review.PrReviewHit;
+
+public class PrReviewHitMapper {
+
+    public static PrReviewHit toModel(PrReviewHitEntity entity) {
+        return PrReviewHit.builder()
+            .id(entity.getId())
+            .prReviewId(entity.getPrReviewId())
+            .userId(entity.getUserId())
+            .lastIncreasedAt(entity.getLastIncreasedAt())
+            .build();
+    }
+
+    public static PrReviewHitEntity toEntity(PrReviewHit model) {
+        return PrReviewHitEntity.builder()
+            .id(model.getId())
+            .prReviewId(model.getPrReviewId())
+            .userId(model.getUserId())
+            .lastIncreasedAt(model.getLastIncreasedAt())
+            .build();
+    }
+}
