a803952b4a5b4d0f05285eb1ce5fa43d06b4e1dd
zakie <realzzaekkii@gmail.com>
2025-12-12 03:48:24 +0900

    feat: 조회기록 repository 인터페이스 추가

---
+++ Moyoy-Infra/src/main/java/com/moyoy/infra/database/mysql/pr_review/PrReviewHitJpaRepository.java
@@ -0,0 +1,5 @@
+package com.moyoy.infra.database.mysql.pr_review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrReviewHitJpaRepository extends JpaRepository<PrReviewHitEntity, Long> {}
